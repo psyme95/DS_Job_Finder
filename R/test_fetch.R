0ce5a9c11e4157790214aed9de6a942e54af6751
psyme95 <psyme95@gmail.com>
2026-02-13 21:26:26 +0000

    Add script to test basic functionality

--- R/test_fetch.R
+++ R/test_fetch.R
@@ -5,12 +5,7 @@ source("R/01_fetch_jobs.R")
 # Fetch jobs
 jobs <- fetch_all_jobs()
 
-# Show summary
-cat("\n=== SUMMARY ===\n")
-cat("Total jobs:", nrow(jobs), "\n")
-cat("From Adzuna:", sum(jobs$source == "adzuna"), "\n")
-cat("From Reed:", sum(jobs$source == "reed"), "\n")
-cat("\n=== FIRST 3 JOBS ===\n")
+# Show first 3 jobs
 print(jobs[1:min(3, nrow(jobs)), c("title", "company", "location", "salary_min")])
 
 
